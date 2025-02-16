2fc77269c1a75a0479f5897e93592ed1e6427226
Thorge Petersen <petersen@rz.uni-kiel.de>
2025-02-16 18:09:03 +0100

    Init theme in app template

--- src/app.vue
+++ src/app.vue
@@ -5,7 +5,12 @@
   </NuxtLayout>
 </template>
 <script setup lang="ts">
+const themeStore = useThemeStore();
+
 onMounted(async () => {
   console.log("Mounted app");
+
+  // Initialize the theme on component mount
+  themeStore.initializeTheme();
 });
 </script>
