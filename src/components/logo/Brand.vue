d8a5508ca11abcc39b8ba305663f05ae72da0293
Thorge Petersen <petersen@rz.uni-kiel.de>
2025-02-17 09:58:58 +0100

    Fixed brand name

--- src/components/logo/Brand.vue
+++ src/components/logo/Brand.vue
@@ -2,7 +2,7 @@
   <div class="logo">
     <NuxtLink :to="localePath('/')" class="flex-0 btn btn-ghost gap-1 px-2 md:gap-2">
       <img src="/assets/images/logo.png" alt="Brand Logo" class="h-5 md:h-6" />
-      <span class="font-title text-base-content text-lg md:text-xl">DhD AG – gwFDM</span>
+      <span class="font-title text-base-content text-lg md:text-xl">DHd AG – gwFDM</span>
     </NuxtLink>
   </div>
 </template>
